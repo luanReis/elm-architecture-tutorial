2f5d698b7d6789be9ea653575598d2f4c65cbfc5
Luan Reis <luan.rpsantos@gmail.com>
2017-02-28 21:22:07 +0100

    Add dice which can be rolled

--- examples/04-random.elm
+++ examples/04-random.elm
@@ -1,5 +1,8 @@
 import Html exposing (..)
 import Html.Events exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import List exposing (..)
 import Random
 
 
@@ -18,13 +21,14 @@ main =
 
 
 type alias Model =
-  { dieFace : Int
+  { firstDieFace : Int
+  , secondDieFace : Int
   }
 
 
 init : (Model, Cmd Msg)
 init =
-  (Model 1, Cmd.none)
+  (Model 1 1, Cmd.none)
 
 
 
@@ -33,18 +37,21 @@ init =
 
 type Msg
   = Roll
-  | NewFace Int
+  | FirstDieNewFace Int
+  | SecondDieNewFace Int
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Roll ->
-      (model, Random.generate NewFace (Random.int 1 6))
+      (model, Random.generate FirstDieNewFace (Random.int 1 6))
 
-    NewFace newFace ->
-      (Model newFace, Cmd.none)
+    FirstDieNewFace newFace ->
+      ({model | firstDieFace = newFace }, Random.generate SecondDieNewFace (Random.int 1 6))
 
+    SecondDieNewFace newFace ->
+      ({model | secondDieFace = newFace }, Cmd.none)
 
 
 -- SUBSCRIPTIONS
@@ -62,6 +69,67 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   div []
-    [ h1 [] [ text (toString model.dieFace) ]
-    , button [ onClick Roll ] [ text "Roll" ]
+    [ dieFaceSvg model.firstDieFace
+    , dieFaceSvg model.secondDieFace
+    , button [ onClick Roll ] [ Html.text "Roll" ]
     ]
+
+
+dieFaceSvg : Int -> Html Msg
+dieFaceSvg dieFace =
+  svg [ width "100", height "100" ]
+      ( List.append [ rect [ fill "white"
+                          , strokeWidth "3"
+                          , stroke "grey"
+                          , x "1"
+                          , y "1"
+                          , width "97"
+                          , height "97"
+                          , rx "15"
+                          , ry "15"
+                          ]
+                          []
+                   ] (dieFaceCircles dieFace)
+      )
+
+
+dieFaceCircles : Int -> List (Svg Msg)
+dieFaceCircles dieFace =
+  List.map dieFaceCircle (dieFaceCirclesPosition dieFace)
+
+
+dieFaceCircle : { cx : String, cy : String } -> Svg Msg
+dieFaceCircle position =
+  circle [ fill "grey", cx position.cx, cy position.cy, r "10" ] []
+
+
+dieFaceCirclesPosition : Int -> List { cx : String, cy : String }
+dieFaceCirclesPosition dieFace =
+  case dieFace of
+    1 -> [ { cx = "49", cy = "49"}
+         ]
+    2 -> [ { cx = "19", cy = "79"}
+         , { cx = "79", cy = "19"}
+         ]
+    3 -> [ { cx = "19", cy = "79"}
+         , { cx = "49", cy = "49"}
+         , { cx = "79", cy = "19"}
+         ]
+    4 -> [ { cx = "19", cy = "19"}
+         , { cx = "19", cy = "79"}
+         , { cx = "79", cy = "19"}
+         , { cx = "79", cy = "79"}
+         ]
+    5 -> [ { cx = "19", cy = "19"}
+         , { cx = "19", cy = "79"}
+         , { cx = "49", cy = "49"}
+         , { cx = "79", cy = "19"}
+         , { cx = "79", cy = "79"}
+         ]
+    _ -> [ { cx = "19", cy = "19"}
+         , { cx = "19", cy = "79"}
+         , { cx = "19", cy = "49"}
+         , { cx = "79", cy = "49"}
+         , { cx = "79", cy = "19"}
+         , { cx = "79", cy = "79"}
+         ]
