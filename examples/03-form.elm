68c51a7416880945c136a42fa3577331594a7f1f
Luan Reis <luan.rpsantos@gmail.com>
2017-02-28 18:40:00 +0100

    Add password and age validation

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,6 +1,8 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
+import Char exposing (isDigit, isUpper, isLower)
+import String exposing (all, any, length)
 
 
 main =
@@ -17,14 +19,16 @@ main =
 
 type alias Model =
   { name : String
+  , age : String
   , password : String
   , passwordAgain : String
+  , validation : (Bool, String)
   }
 
 
 model : Model
 model =
-  Model "" "" ""
+  Model "" "" "" "" (False, "")
 
 
 
@@ -33,8 +37,10 @@ model =
 
 type Msg
     = Name String
+    | Age String
     | Password String
     | PasswordAgain String
+    | Validate
 
 
 update : Msg -> Model -> Model
@@ -43,12 +49,35 @@ update msg model =
     Name name ->
       { model | name = name }
 
+    Age age ->
+      { model | age = age }
+
     Password password ->
       { model | password = password }
 
     PasswordAgain password ->
       { model | passwordAgain = password }
 
+    Validate ->
+      { model | validation = validate model }
+
+
+validate : Model -> (Bool, String)
+validate model =
+  if not (all isDigit model.age) then
+    (False, "Age must be a number!")
+  else if not (model.password == model.passwordAgain) then
+    (False, "Passwords do not match!")
+  else if not (any isUpper model.password) then
+    (False, "Password should contain at least 1 upper case character!")
+  else if not (any isLower model.password) then
+    (False, "Password should contain at least 1 lower case character!")
+  else if not (any isDigit model.password) then
+    (False, "Password should contain at least 1 numeric character!")
+  else if not (length model.password > 8) then
+    (False, "Password should be longer than 8 characters!")
+  else
+    (True, "OK")
 
 
 -- VIEW
@@ -58,19 +87,24 @@ view : Model -> Html Msg
 view model =
   div []
     [ input [ type_ "text", placeholder "Name", onInput Name ] []
+    , input [ type_ "text", placeholder "Age", onInput Age ] []
     , input [ type_ "password", placeholder "Password", onInput Password ] []
     , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , button [ onClick Validate ] [ text "Submit" ]
     , viewValidation model
     ]
 
 
-viewValidation : Model -> Html msg
+viewValidation : Model -> Html Msg
 viewValidation model =
   let
     (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
+      let (valid, message) =
+        model.validation
+      in
+        if valid then
+          ("green", message)
+        else
+          ("red", message)
   in
     div [ style [("color", color)] ] [ text message ]
